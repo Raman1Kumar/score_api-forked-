425cb7e7fc168633c19286f0bd664b2fa6c5f30e
Raman1Kumar <kumraman555@gmail.com>
2023-01-28 15:23:33 +0530

    updated index port 3000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 var allowedOrigins = [
   "http://localhost:8080",
